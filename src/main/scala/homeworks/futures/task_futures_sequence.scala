1653b3d857d284c3a627feb9b1fb1fd102ccfe02
kobelev.ilya@gmail.com <Ilya.Kobelev@acronis.com>
2020-12-22 00:03:07 +0300

    HW - Futures

--- src/main/scala/homeworks/futures/task_futures_sequence.scala
+++ src/main/scala/homeworks/futures/task_futures_sequence.scala
@@ -2,7 +2,9 @@ package homeworks.futures
 
 import homeworks.HomeworksUtils.TaskSyntax
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.Nil
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object task_futures_sequence {
 
@@ -12,14 +14,22 @@ object task_futures_sequence {
    * возвращающую все успешные и не успешные результаты.
    * Возвращаемое тип функции - кортеж из двух списков,
    * в левом хранятся результаты успешных выполнений,
-   * в правово результаты неуспешных выполнений.
-   * Не допускается использование методов объекта Await и мутабельных переменных var
+   * в правово результаты неуспешных выполнений
    */
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
-  def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`"()
+  def fullSequence[A](futures: List[Future[A]]): Future[(List[A], List[Throwable])] = {
+    val fs = futures.map(f => f.recover { case ex => ex })
+    Future.sequence(fs)
+      .map(l => l.foldLeft((Nil: List[A], Nil: List[Throwable])) {
+        (acc, r) =>
+          r match {
+            case r: Throwable => (acc._1, r :: acc._2)
+            case r: A => (r :: acc._1, acc._2)
+          }
+      })
+      .map(t => (t._1.reverse, t._2.reverse))
+  }
 }
